70d5bbc8e1fe8032986a8fd6c9b2c348eb81f5d8
MSalman3428 <msalman3428@github.com>
2026-09-01 04:16:52 +0500

    code change for images and git repos

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,17 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+// Replace 'Portfolio' with your exact GitHub repository name
+const repoName = "Portfolio"; 
+
 const nextConfig: NextConfig = {
   output: "export",
   images: {
-    unoptimized: true,
+    unoptimized: true, // Required for static exports on GitHub Pages
   },
+  // Automatically sets the subfolder path when building on GitHub Actions
+  basePath: isGithubActions ? `/${repoName}` : "",
+  assetPrefix: isGithubActions ? `/${repoName}/` : "",
   turbopack: {
     root: __dirname,
   },
